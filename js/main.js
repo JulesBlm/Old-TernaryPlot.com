e8396569044a942138853174fa8288bd536d1a94
Jules <julesblom@gmail.com>
2018-07-28 15:25:30 +0200

    Handsontable

--- js/main.js
+++ js/main.js
@@ -1,26 +1,19 @@
 /* To do
 
---------BUGS--------
-1. Fix when only one line/area is present followed by a '---'
-2. https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
-3. Catch errors
-4. Filter out emplty arrays split dashes
+ADD rows!
+0. Ignore empty rows for points and split for points/areas
+1. columns widths table
+2. localStorage
+3. Make function to construct HOTtable for each type MODULARIZE
+4. Default value renderer
+5. https://docs.handsontable.com/4.0.0/demo-data-validation.html
 
-2. On resize mobile
-1. Column order agnostic
-    Check column index between lines and point [global variable?]
-    Look for words in columns that are not keywords and make those the vertexlabels
+Explanation in firefox doesnt work
 
-    keyword1Index: 3
-    keyword2Index: 1
-    keyword3Index: 2
-3. Update to d3 v5
-5. Upload and submit csv
-
-- cookie: First visit example data, afterwards keep entered text or LocalStorage?
---------MAKE IT SLICKER--------
-- validate input
-- more error handling
+https://spin.atomicobject.com/2014/01/21/convert-svg-to-png/
+On resize mobile
+Update to d3 v5
+Upload and submit csv
 - option for radius/size of point
 - lines
     - end style: arrow-end) https://vanseodesign.com/web-design/svg-markers/
@@ -29,28 +22,99 @@
     http://jstherightway.org/#js-code-style
     https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects
   https://ejb.github.io/2017/08/09/a-better-way-to-structure-d3-code-es6-version.html
-
---------Later Features--------
-- hexbin option
-- areas
-- heatmap & contour option
-- Curved lines
 */
 
-let defaultPointColor = "black";
-let defaultShape = "circle";
 
-let defaultLinestyle = "none";
-let defaultLineColor = "black";
-let defaultStrokewidth = "4px";
+// ??? Big ass 'case' functon because there are only three typse anyway ???
+// ??? height and width as function of pagesize ???
+function HandsOnTableCreator(ID, sampleData) {
+  return new Handsontable(ID, {
+    data: sampleData,
+    fixedRowsTop: 1,
+    manualColumnMove: true,
+    manualRowMove: true,  
+    rowHeaders: true,
+    colHeaders: true,
+    minRows: 100,
+    height: 300,
+    width: 500,
+    dropdownMenu: true,
+    manualColumnResize: true
+  });  
+}
 
-let defaultAreaColor = "grey";
-let defaultAreaOpacity = 0.5;
-
-// const items = JSON.parse(localStorage.getItem("points")) || [];
 labelsAdded = false;
-
 let columns;
+
+var pointsSampledata = [
+  ["Sand", "Silt", "Clay", "Color", "Shape", "Title"],
+  [0.3, 0.3, 0.4, "limegreen", "", "Sample Nr 1"],
+  [1,0,0],
+  [0,1,0],
+  [0,0,1],
+  [0.2,0.5,0.3,"coral"],
+  [0.3, 0.1, 0.6,"magenta","cross"],
+  [0.5,0.5,0,"#d1b621","diamond"],
+  [0.6,0.2,0.2,"peru","triangle-up"]
+];
+
+const pointsTable = HandsOnTableCreator(document.getElementById("pointsTable"), pointsSampledata);
+
+const submitPointsButton = document.enterPoints;
+
+Handsontable.dom.addEvent(submitPointsButton, "submit", function(e) {
+  e.preventDefault();
+
+  let pointsData = pointsTable.getData();
+  const parsedPoints = parsePoints(pointsData);
+  drawPoints(parsedPoints);
+});
+
+var linesSampledata = [
+  ["Sand", "Silt", "Clay", "Color", "Linestyle", "Strokewidth", "Title"],
+  [0.2,0.8,0,"orangered"], //"5 3 5",2,"dotted line 1"],
+  [0.8,0,0.2,],
+  [],
+  [0.1,0.1,0.8,"slateblue"],
+  [0.4,0.1,0.5],
+  [0,0,1]
+];
+
+const linesTable = HandsOnTableCreator(document.getElementById("linesTable"), linesSampledata);
+const submitLinesButton = document.enterLines;
+Handsontable.dom.addEvent(submitLinesButton, "submit", function(e) {
+  e.preventDefault();
+
+  const linesData = linesTable.getData();
+  const parsedLines = parseLinesAreas(linesData);
+
+  drawLines(parsedLines);
+});
+
+const areasSampledata = [
+  ["Sand","Silt","Clay","Color","Opacity","Title"],
+  [0,0.5,0.5,"palegreen",0.1,"More than 50% silt"],
+  [0.5,0.5,0],
+  [0,1,0],
+  [],
+  [0.5,0,0.5,"moccasin",0.4,"More than 50% sand"],
+  [0.5,0.5,0],
+  [1,0,0],
+  [],
+  [0,0.5,0.5,"coral",0.3,"More than 50% clay"],
+  [0.5,0,0.5],
+  [0,0,1]
+];
+
+var areasTable = HandsOnTableCreator(document.getElementById("areasTable"), areasSampledata);
+const submitAreasButton = document.enterAreas;
+Handsontable.dom.addEvent(submitAreasButton, "submit", function(e) {
+  e.preventDefault();
+
+  let areasData = areasTable.getData();
+  const parsedAreas = parseLinesAreas(areasData);
+  drawAreas(parsedAreas);
+});
 
 function capitalize(word) {
   return word.toLowerCase().replace(/\b[a-z]/g, function(letter) {
@@ -75,141 +139,78 @@ const ternary = d3.ternary.plot()
   .call(d3.ternary.neatline())
   .call(graticule);
 
+
 d3.select("#ternary-plot").call(ternary);
 
-/* ------ Points ------ */
-function drawPoints(d) {
-  const values = d.slice([0, 3]);
-  const symbol = d3.svg.symbol();
-  let myValues;
+// ??? One parse function for all types ???
+// ??? break up into smaller functions
 
-  const points = ternary.plot()
-    .selectAll(".point")
-    .data(values);
-
-  points.enter().append("path")
-      .attr("class", "point")
-      .attr("fill",function(point) { return point.color ? (point.color).trim() : (point.colour ? (point.colour).trim() : defaultPointColor)}) // both color and colour are valid
-      .attr("fill-opacity", function(point) { return point.opacity ?  point.opacity.trim() : 1 }) 
-      .attr("d", symbol.type(function(point) { return point.shape ? (point.shape).trim() : defaultShape; }))
-      .attr("transform", function(point) {
-        const myKeys = Object.keys(point);
-        myValues = [point[myKeys[0]], point[myKeys[2]], point[myKeys[1]]];
-
-        const plotCoords = ternary.point(myValues);
-        return "translate(" + plotCoords[0] + "," + plotCoords[1] + ")";
-      })
-      .on("mouseover", handleMouseOver)
-      .on("mouseout", handleMouseOut)
-    .append("title")
-      .text( function(point) { 
-        const myKeys = Object.keys(point);
-        const valuesString = `${capitalize(myKeys[0])}: ${point[myKeys[0]]}, ${capitalize(myKeys[2])}: ${point[myKeys[2]]}, ${capitalize(myKeys[1])}: ${point[myKeys[1]]}`;
-        return point.title ? `${capitalize(point.title.trim())}; ${valuesString}` : valuesString;
-      });
-}
-
-/* ------ Lines ------ */
-function drawLines(d) {
-  // console.log("----------------------------------------------");
-  // console.log("DRAWING", d);
-  // console.log("----------------------------------------------");
-
-  const paths = ternary.plot()
-    .selectAll(".line")
-    .data(d);
-
-  // 🤔I think there must be a way to do this 'better' with Object methods
-  paths.enter().append("path")
-      .attr("class", "ternary-line" )
-      .attr("d", function(line) {
-        // Use d3.pairs?
-        let drawArray = [];
-        const myKeys = Object.keys(line[0]);
-        // Loop over each point in line and add to drawarray because d3 path wants it that way
-        for (let i = 0; i <= (line.length - 1); i+=1) {
-          // d3.ternary wants the values swapped ¯\_(ツ)_/¯
-          const current = [+line[i][myKeys[0]], +line[i][myKeys[2]], +line[i][myKeys[1]]]; // Better find the index of the columns that aren"t keywords
-          drawArray.push(current);
-        };
-        return ternary.path(drawArray);
-      })
-      .attr("stroke-dasharray", function(line) { return line[0].linestyle ?  line[0].linestyle.trim().replace("/([\s])+/", ",") : defaultLinestyle })
-      .attr("stroke", function(line) { return line[0].color ? (line[0].color).trim() : (line[0].colour ? (line[0].colour).trim() : defaultLineColor)}) // both color and colour are valid   
-      .attr("stroke-opacity", function(line) { return line[0].opacity ?  line[0].opacity.trim() : 1 })
-      .attr("fill-opacity", "0") // So no inside fill shows up inside lines in Adobe Illustrator 
-      .attr("stroke-width", function(line) { return line[0].strokewidth ? line[0].strokewidth : defaultStrokewidth })
-      .append("title") // 🤔 Would there be a way to not append a title if there is none?
-        .text( function(line) { return line[0].title ? capitalize((line[0].title).trim()) : undefined; }); //Object.values(e).slice(0,3).join(", ")
-}
-
-/* ------ Areas ------ */
-function drawAreas(d) {
-  const paths = ternary.plot()
-    .selectAll(".area")
-    .data(d);
-
-  paths.enter().append("path")
-      .attr("class", "ternary-area" )
-      .attr("d", function(line) {
-        let drawArray = [];
-        const myKeys = Object.keys(line[0]);
-        for (let i = 0; i <= (line.length - 1); i+=1) {
-          const current = [+line[i][myKeys[0]], +line[i][myKeys[2]], +line[i][myKeys[1]]];
-          drawArray.push(current);
-        };
-        return ternary.area(drawArray);
-      })
-      .attr("fill", function(area) { return area[0].color ? (area[0].color).trim() : defaultAreaColor })
-      .attr("fill-opacity", function(area) { return area[0].opacity ?  area[0].opacity.trim() : defaultAreaOpacity })
-      .append("title")
-        .text( function(area) { return area[0].title ? capitalize((area[0].title).trim()) : undefined; });
-}
-
-function submittedPoints(e) {
-  e.preventDefault();
-  const parsedInput = d3.csvParse((this.querySelector("[name=item]")).value.toLowerCase());
-
-  parsedInput.columns = parsedInput.columns.map(col => col.trim());
-
+// Check if lines column names match with previous column names
+function checkColumns(columnNames) {
   if (!columns) {
-    columns = parsedInput.columns.slice(0,3).map(col => col.trim());
+    columns = columnNames.slice(0,3);
   } else {
-    if (JSON.stringify(parsedInput.columns.slice(0,3)) !== JSON.stringify(columns)) swal("Your columns in Points and Lines don't seem to match", `Your columns for points are "${parsedInput.columns.slice(0,3)}" and for lines they are "${columns}". . The point will still be plotted, but they might appear the way you intended.`, "warning");
-  }
+    if (JSON.stringify(columnNames.slice(0,3)) !== JSON.stringify(columns)) {
+      swal("Your columns in Points, Lines and areas don't seem to match", `Your columns you entered first are "${columns}" and for your columns now are "${columnNames.slice(0,3)}". Your data will still be plotted, but it might not appear the way you intended.`, "warning");
+    }
+  };
 
-  columns.some(v => { if (reserved.includes(v)) { swal("Reserved column name", `You can't use any of the following names as your columns names: ${reserved.join(', ')}`, "error") }; return }) 
-
-  if (!labelsAdded) { addVertexLabels(parsedInput); labelsAdded = true;}
-  drawPoints(parsedInput);
+  // Check for reserved keywords in column names
+  columnNames.some(key => { if (reserved.includes(key)) {
+        swal("Reserved column name", `You can't use any of the following names as your columns names: ${reserved.join(", ")}`, "error") };
+        return;
+  })
 }
 
-function parse(data) {
-  let splitNewlines = data.split(/([-])+/).map(d => d.split("\n")); // Split by dashes [separate lines to draw], then split by newlines [separate points in each line]
-  
-  const columnsString = splitNewlines[0].shift(); // Remove first entry (Columns)
-  const columnsArray = columnsString.split(",").map(col => col.trim()); // Array with column names
+function parsePoints(data) {
+  let lines = data;
+  const columnsArray = lines.shift(); // Construct an array with column names
 
-  // Check if lines column names match with point column names
-  if (!columns) {
-    columns = columnsArray.slice(0,3)
-  } else {
-    if (JSON.stringify(columnsArray.slice(0,3)) !== JSON.stringify(columns)) {
-      swal("Your columns in Points, Lines and areas don't seem to match", `Your columns you entered first are "${columns}" and for your columns now are "${columnsArray.slice(0,3)}". Your data will still be plotted, but it might not appear the way you intended.`, "warning");
+  checkColumns(columnsArray);
+
+  // Filter out empty/null entries
+  lines = lines.map(arr => arr.filter(entry => entry !== null));
+  lines = lines.filter(arr => arr.length !== 0);
+
+  // Add labels to ternary plot
+  if (!labelsAdded) { addVertexLabels({columns: columnsArray}); labelsAdded = true;}
+
+  // Construct array of object with properties for drawing
+  // Its ugly but it works ¯\_(ツ)_/¯
+  const objectsArray = lines.map(line => {
+      const point = columnsArray.reduce(function(result, column, i) {
+        result[column.toLowerCase()] = line[i];
+        return result;
+      }, {});
+      return point
+    });
+
+  return objectsArray
+}
+
+function parseLinesAreas(data) {
+  let lines = data;
+  const columnsArray = lines.shift();
+
+  checkColumns(columnsArray);
+
+  lines = lines.map(arr => arr.filter(entry => entry !== null)); // Filter out empty/null entries
+  const drawLines = [];
+
+  let drawLine = [];
+  // Loop over lines array
+  for (i = 0; i < lines.length; i++) {
+    // Check if entry is not empty
+    if (lines[i].length !== 0) {
+      // Add to drawLine
+      drawLine.push(lines[i]);
+    } else if (lines[i].length === 0) {
+      if (drawLine.length !== 0) { drawLines.push(drawLine); }
+      drawLine = []
     }
   }
 
-  columns.some(v => { if (reserved.includes(v)) { swal("Reserved column name", `You can't use any of the following names as your columns names: ${reserved.join(', ')}`, "error") }; return }) 
-
-  splitNewlines = splitNewlines.map(arr => arr.filter(entry => String(entry) !== "")); // Filter out empty entries
-  // splitNewlines = splitNewlines.map(arr => arr.filter(entry => entry !== /([-])+/)); // Filter out dashed
-
-  const lines = splitNewlines.map(point => point.map(value => value.split(",")) );
-
-  if (!labelsAdded) { addVertexLabels({columns: columnsArray}); labelsAdded = true;}
-
-  // Its ugly but it works ¯\_(ツ)_/¯
-  const objectsArray = lines.map(line => {
+  const objectsArray = drawLines.map(line => {
     const lineObjects = line.map(function(p) {
       const point = columnsArray.reduce(function(result, column, i) {
         result[column.toLowerCase()] = p[i];
@@ -221,25 +222,6 @@ function parse(data) {
   });
 
   return objectsArray
-}
-
-function submittedLines(e) {
-  e.preventDefault();
-
-  const rawInput = (this.querySelector("[name=item]")).value;
-  const lineObjectsArray = parse(rawInput);
-
-  drawLines(lineObjectsArray);
-}
-
-function submittedAreas(e) {
-  e.preventDefault();
-
-  const rawInput = (this.querySelector("[name=item]")).value;
-
-  const areaObjectsArray = parse(rawInput);
-
-  drawAreas(areaObjectsArray);
 }
 
 function handleMouseOver(e) { 
@@ -320,10 +302,7 @@ function clearAll(e) {
   clearAreas();
 }
 
-/* event listeners */
-document.enterPoints.addEventListener("submit", submittedPoints);
-document.enterLines.addEventListener("submit", submittedLines);
-document.enterAreas.addEventListener("submit", submittedAreas);
+const timeOutTime = 600;
 
 const clearPointsButton = document.getElementById("clearPoints");
 clearPointsButton.addEventListener("click", clearPoints);
@@ -334,19 +313,19 @@ clearPointsButton.addEventListener("mouseover", function(event) {
   setTimeout(function() {
     d3.selectAll(".point")
       .attr("opacity", "1");
-  }, 600);  
+  }, timeOutTime);  
 });
 
 const clearLinesButton = document.getElementById("clearLines");
 clearLinesButton.addEventListener("click", clearLines);
 clearLinesButton.addEventListener("mouseover", function(event) {
-  d3.selectAll(".ternary-area")
+  d3.selectAll(".ternary-line")
     .attr("stroke-opacity", "0.3");
 
   setTimeout(function() {
-    d3.selectAll(".ternary-area")
+    d3.selectAll(".ternary-line")
       .attr("stroke-opacity", "1");
-  }, 600);  
+  }, timeOutTime);  
 });
 
 const clearAreasButton = document.getElementById("clearAreas");
@@ -359,7 +338,7 @@ clearAreasButton.addEventListener("mouseover", function(event) {
   setTimeout(function() {
     d3.selectAll(".ternary-area")
       .attr("fill-opacity", "1");
-  }, 600);  
+  }, timeOutTime);  
 });
 
 const clearLabelsButton = document.getElementById("clearLabels");
@@ -372,12 +351,26 @@ clearLabelsButton.addEventListener("mouseover", function(event) {
   setTimeout(function() {
     d3.selectAll(".vertex-label")
       .attr("text-decoration", "none");
-  }, 700);
+  }, timeOutTime);
 })
 
 const clearAllButton = document.getElementById("clearAll");
 clearAllButton.addEventListener("click", clearAll);
+clearAllButton.addEventListener("mouseover", function(event) {
+  d3.selectAll(".ternary-line,.vertex-label,.ternary-area,.point")
+    .attr("fill-opacity", "0.3")
+    .attr("text-decoration", "line-through")
+    .attr("stroke-opacity", "0.3");
 
+  setTimeout(function() {
+    d3.selectAll(".ternary-line,.vertex-label,.ternary-area,.point")
+        .attr("fill-opacity", "1")
+        .attr("text-decoration", "none")
+        .attr("stroke-opacity", "1");
+  }, timeOutTime);
+})
+
+/* Set default parameters */
 document.querySelector(`select[name="defaultColorPoints"]`).onchange = function() { defaultPointColor = event.target.value; };
 document.querySelector(`select[name="defaultShape"]`).onchange = function() { defaultShape = event.target.value; };
 
