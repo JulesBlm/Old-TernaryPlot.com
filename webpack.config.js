f07e2f6cd3911eb23aacf3c1c3a72029233ff7c3
JulesBlm <julesblom@gmail.com>
2018-08-10 16:29:34 +0200

    webpack version

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+    devtool: 'inline-source-map',    
+    entry: {
+       app: './src/js/main.js',
+       explain: './src/js/explain.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist/js')
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins: [
+        // Ignore all locale files of moment.js
+        new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+        // new webpack.ProvidePlugin({Reveal: 'reveal.js',}),
+    ],
+ optimization: {
+    runtimeChunk: false,
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+};
